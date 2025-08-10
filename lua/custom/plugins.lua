28a7ce494814581040d9f6490ea94edab927efba
Julius <julius.paramono@gmail.com>
2025-08-10 06:13:33 -0700

    add changes for c++ to work

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,11 +1,17 @@
 return {
   {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require(".configs.null-ls")
+    end,
+  },
+  {
     "christoomey/vim-tmux-navigator",
     lazy = false,
   },
   {
-    "github/copilot.vim",
-    lazy = false,
+    "github/copilot.vim", lazy = false,
   },
 
   {
