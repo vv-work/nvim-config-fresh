6dfa4ebb878cb7def59a293c3640854f65c51a61
Julius <julius.paramono@gmail.com>
2025-08-17 12:17:16 -0700

    Before distruction

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -52,7 +52,8 @@ return {
       vim.g.mkdp_refresh_slow = 0
       vim.g.mkdp_command_for_global = 0
       vim.g.mkdp_open_to_the_world = 0
-      vim.g.mkdp_browser = "open"
+      vim.g.mkdp_browser = "/usr/bin/open"
+      vim.g.mkdp_browserfunc = ""
       vim.g.mkdp_echo_preview_url = 1
       vim.g.mkdp_page_title = "Preview: ${name}"
     end,
