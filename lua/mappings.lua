03dafbb23de51bc51e01f5db83dcde7486c8e330
Julius <julius.paramono@gmail.com>
2025-08-22 02:51:43 -0700

    last changes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,21 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- nvim-surround keymaps (already configured in plugin setup, this is for reference)
+-- Normal mode:
+-- ys<motion><char> - Add surroundings (e.g., ysiw" adds quotes around word)
+-- yss<char> - Add surroundings around entire line
+-- yS<motion><char> - Add surroundings on new lines
+-- ySS<char> - Add surroundings around entire line on new lines
+-- ds<char> - Delete surroundings (e.g., ds" removes quotes)
+-- cs<old><new> - Change surroundings (e.g., cs"' changes quotes to single quotes)
+-- cS<old><new> - Change surroundings on new lines
+
+-- Visual mode:
+-- S<char> - Add surroundings around selection
+-- gS<char> - Add surroundings around selection on new lines
+
+-- Insert mode:
+-- <C-g>s<char> - Add surroundings
+-- <C-g>S<char> - Add surroundings on new lines
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -667,6 +667,30 @@ return {
     end,
   },
 
+  -- surround plugin
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        keymaps = {
+          insert = "<C-g>s",
+          insert_line = "<C-g>S",
+          normal = "ys",
+          normal_cur = "yss",
+          normal_line = "yS",
+          normal_cur_line = "ySS",
+          visual = "S",
+          visual_line = "gS",
+          delete = "ds",
+          change = "cs",
+          change_line = "cS",
+        },
+      })
+    end,
+  },
+
   -- test new blink
   { import = "nvchad.blink.lazyspec" },
 
